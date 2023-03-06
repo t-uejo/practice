846c1dd69f45523e8627068969b7fe12debbc876
uejo <info@taikiuejo.com>
2023-03-06 22:40:21 +0900

    spring-initializrを使ってプロジェクトの雛形を作成 #2

---
+++ issue-tracking/src/main/java/com/example/issuetracking/IssueTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.issuetracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackingApplication.class, args);
+	}
+
+}
