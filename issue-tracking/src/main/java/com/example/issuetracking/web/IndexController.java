8c7487d173ec26c2cc81c90f0b17cd41d8836779
uejo <info@taikiuejo.com>
2023-03-07 20:33:47 +0900

    feat：テンプレートエンジンを用いてindex.htmlを返すコントローラークラスを作成 #2

---
+++ issue-tracking/src/main/java/com/example/issuetracking/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.issuetracking.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index.html";
+    }
+}
