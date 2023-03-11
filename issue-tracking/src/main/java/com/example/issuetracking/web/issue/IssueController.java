a49f7196f1e2850488e4863237516d262db3825b
uejo <info@taikiuejo.com>
2023-03-11 09:42:51 +0900

    feat：Spring BootのDIコンテナを使ってBean登録とDIを行った #2

--- issue-tracking/src/main/java/com/example/issuetracking/web/issue/IssueController.java
+++ issue-tracking/src/main/java/com/example/issuetracking/web/issue/IssueController.java
@@ -2,6 +2,8 @@ package com.example.issuetracking.web.issue;
 
 import com.example.issuetracking.domain.issue.IssueEntity;
 import com.example.issuetracking.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,14 @@ import java.util.List;
 
 @Controller
 public class IssueController {
-    private final IssueService issueService = new IssueService();
+
+    private final IssueService issueService;
+
+    @Autowired
+    public IssueController(IssueService issueService) {
+        this.issueService = issueService;
+    }
+
     @GetMapping("/issues")
     public String showList(Model model){
         var issueList = issueService.findAll();
