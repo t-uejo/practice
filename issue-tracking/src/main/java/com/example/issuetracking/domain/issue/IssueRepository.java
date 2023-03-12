ae0ded79d76fdc31a9e8d449f0b73dac9421c4b6
uejo <info@taikiuejo.com>
2023-03-12 21:26:26 +0900

    feat：Mybatisでマッピングを行い、DBからデータ取得できるようにした #2

---
+++ issue-tracking/src/main/java/com/example/issuetracking/domain/issue/IssueRepository.java
@@ -0,0 +1,12 @@
+package com.example.issuetracking.domain.issue;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface IssueRepository {
+    @Select("select * from issues")
+    List<IssueEntity> findAll();
+}
--- issue-tracking/src/main/java/com/example/issuetracking/domain/issue/IssueService.java
+++ issue-tracking/src/main/java/com/example/issuetracking/domain/issue/IssueService.java
@@ -1,16 +1,26 @@
 package com.example.issuetracking.domain.issue;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class IssueService {
+    private final IssueRepository issueRepository;
+
+    @Autowired
+    public IssueService(IssueRepository issueRepository) {
+        this.issueRepository = issueRepository;
+    }
+
     public List<IssueEntity> findAll() {
-        return List.of(
-                new IssueEntity(1, "概要１", "説明１"),
-                new IssueEntity(2, "概要２", "説明２"),
-                new IssueEntity(3, "概要３", "説明３")
-        );
+        return issueRepository.findAll();
+
+//        return List.of(
+//                new IssueEntity(1, "概要１", "説明１"),
+//                new IssueEntity(2, "概要２", "説明２"),
+//                new IssueEntity(3, "概要３", "説明３")
+//        );
     }
 }
